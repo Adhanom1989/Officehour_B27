17d4301500d4a78bb61fed872e68699dbf04a8c1
Admin <adhanomkst@gmail.com>
2022-09-03 14:20:14 -0500

    Initial commit

---
+++ src/day06_08_03_2022/MyCalculatorTest.java
@@ -0,0 +1,12 @@
+package day06_08_03_2022;
+
+public class MyCalculatorTest {
+    public static void main(String[] args) {
+        //if you want to call one method from different class
+        //we need to use ClassName.MethodName
+        System.out.println("MyCalculator.add(5, 4) = " + MyCalculator.add(5, 4));
+
+        String name="Adam";
+        System.out.println(name);
+    }
+}
